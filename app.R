c99ee2c7ecbb0429947ae32aaf7228a870008951
Rems <remi.vergnon@free.fr>
2017-01-17 11:23:15 +0000

    refined graphics and options for app

--- app.R
+++ app.R
@@ -3,7 +3,6 @@ library(leaflet)
 library(merpWS)
 library(merpData)
 library(jsonlite)
-library(magrittr)
 library(ggplot2)
 library(ggmap)
 library(reshape2)
@@ -11,6 +10,8 @@ library(reshape2)
 alldatasets <- get_cefas_datasets()
 alldatasets <- alldatasets$to_download
 param <- alldatasets$Name
+mychoices <- c("SWT02 - Fishing Survey System", "SWT01 - Coastal Temperature Network", "SWT04 - Plankton Analysis System",
+               "SWT06 - SmartBuoy Network", "SWT09 - Data Storage Tag Database")
 
 myfilter <- function(mydata, latrange, lonrange){
   names(mydata) <- tolower(names(mydata))
@@ -21,18 +22,18 @@ myfilter <- function(mydata, latrange, lonrange){
   else{print("No longitude data can be found!"); stop()}
   mydata <- mydata[, c("lat", "lon")]
   mydata <- mydata[mydata$lat > latrange[1] & mydata$lat < latrange[2] & mydata$lon > lonrange[1] & mydata$lon < lonrange[2], ]
-  # mydata[sample(size = nb, x = 1:nrow(mydata), replace = F), ]
   mydata
 }
 
 ui <- fluidPage(
   
+  h2("Spatial distribution of CEFAS temperature data"),
+  
   sidebarLayout(
     sidebarPanel(
-      selectInput("dataset", label = "CEFAS temperature data set", choices = paste("SWT0", c(9, 4, 2), sep = "")),
+      selectInput("dataset", label = "CEFAS temperature data set", choices = mychoices),
       sliderInput("sliderlonrange1", label = "Longitudinal range", min = -20, max = 20, value = c(-10, 10)),
       sliderInput("sliderlatrange1", label = "Latitudinal range", min = 40, max = 80, value = c(50, 70)),
-      # sliderInput("mynumber", label = "number of data points to display", min = 1, max = 500, value = 10),
       width = 4
     ),
     
@@ -41,14 +42,17 @@ ui <- fluidPage(
   
   p(),
   
+  h2("Smoothed time series of CEFAS temperature data"),
+  
   sidebarLayout(
     sidebarPanel(
-      sliderInput("slidertimerange", label = "Time range", min = 1950, max = 2016, value = c(1990, 2010)),
-      sliderInput("sliderlonrange2", label = "Longitudinal range", min = -20, max = 20, value = c(-10, 10)),
+      sliderInput("slidertimerange", label = "Time range", min = 1950, max = 2016, value = c(1980, 2010)),
+      sliderInput("sliderlonrange2", label = "Longitudinal range", min = -20, max = 20, value = c(2, 10)),
       sliderInput("sliderlatrange2", label = "Latitudinal range", min = 40, max = 80, value = c(50, 70)),
-      sliderInput("myresolution", label = "Spatial unit", min = 0.5, max = 10, value = 1),
+      sliderInput("myresolution", label = "Each time series corresponds to a square cell of", min = 0.5, max = 10, value = 1),
+      sliderInput("mysmoother", label = "Smoothing", min = 0.1, max = 2, value = 0.5),
       width = 4
-    ),
+    ), 
     
     mainPanel(
       plotOutput("time_series"))),
@@ -58,12 +62,8 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   
   points <- reactive({
-    download_cefas_data(alldatasets$Id[grep(alldatasets$Name, pattern = input$dataset)]) 
-    # myfilter(res)
-    # attempt at finding coordinates
+    download_cefas_data(alldatasets$Id[grep(alldatasets$Name, pattern = substr(input$dataset, 1, 5))]) 
   })
-  
-  # http://leaflet-extras.github.io/leaflet-providers/preview/
   
   output$mymap <- renderLeaflet({
     leaflet() %>%
@@ -75,19 +75,17 @@ server <- function(input, output, session) {
       # addMarkers(data = myfilter(points(), nb = input$mynumber))
       addCircles(data = myfilter(points(),  latrange = input$sliderlatrange1, lonrange = input$sliderlonrange1),
                  color = "black", fillColor = "blue")
-    # could have addCircles instead?
   })
   
   output$time_series <- renderPlot({
     mydata <- points()
-    # mydata <- download_cefas_data(alldatasets$Id[grep(alldatasets$Name, pattern = "SWT09")]) 
     mytimes <- strsplit(as.character(mydata$Time), split = " ")
     mytimes <- sapply(mytimes, function(x) x[1])
     mytimes <- strsplit(mytimes, split = "/", fixed = T)
     mytimes <- sapply(mytimes, function(x) x[3])
     mydf <- data.frame(mytimes, mydata$tC, mydata$Lat, mydata$Long)
     names(mydf) <- c("mytimes", "tC", "lat", "lon")
-    # for(i in 1:ncol(mydf)) mydf[, i] <- as.numeric(mydf[, i])
+    
     mydf <- mydf[as.numeric(as.character(mydf$mytimes)) > input$slidertimerange[1] & 
                    as.numeric(as.character(mydf$mytimes)) < input$slidertimerange[2], ]
     mydf <- mydf[mydf$lon > input$sliderlonrange2[1] & mydf$lon < input$sliderlonrange2[2], ]
@@ -103,8 +101,11 @@ server <- function(input, output, session) {
     res <- data.frame(year = as.numeric(rownames(res)), res)
     res <- melt(res, id.vars = "year")
     
-    ggplot() + geom_line(data = res, aes(x = year, y = value, col = variable)) +
-      theme(legend.position = "none")
+    ggplot(data = res, aes(x = year, y = value, col = variable)) +
+      geom_smooth(span = input$mysmoother, se = FALSE) +
+      labs(x = "Year", y = "Mean temperature") +
+      theme(legend.position = "none", axis.text = element_text(size = 14), axis.title = element_text(size = 14),
+            panel.grid.minor = element_blank(), panel.background = element_rect(fill = "white"))
   })
   
 }
